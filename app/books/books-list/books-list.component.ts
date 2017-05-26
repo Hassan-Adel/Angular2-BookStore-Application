137e83379ef82540e1fab371256711560ede9a22
Hassan-Adel <hasanadel555@hotmail.com>
2017-05-26 22:30:25 +0200

    Adding BooksListComponent and  -1 way - Data Binding (interpuloation, property binding)

---
+++ app/books/books-list/books-list.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+
+@Component({
+	moduleId: module.id,
+	selector:'bs-books-list',
+	templateUrl: 'books-list.component.html'
+})
+
+export class BooksListComponent {
+	//property binding
+	imageWidth: number = 100;
+	//interpuloation
+	bookAuthor: string = "Tom Jones";
+	bookTitle: string = "War and Peace II";
+	bookPrice: string = "$29.95";
+	bookDescription: string = "Book of historical fiction.";
+	bookReviews: number = 15;
+	bookImageUrl: string = "app/assets/images/656.jpg";
+}
